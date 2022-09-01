6178c21a12fca5a44819cfd256c004cf87a926bd
Binay Shaw <binayshaw7777@gmail.com>
2022-09-01 14:03:31 +0530

    (+) SignIn SignUp UI Done

--- app/src/main/java/com/geekym/knowdrowsy/authentication/SignIn_Activity.java
+++ app/src/main/java/com/geekym/knowdrowsy/authentication/SignIn_Activity.java
@@ -2,15 +2,33 @@ package com.geekym.knowdrowsy.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.geekym.knowdrowsy.R;
 
 public class SignIn_Activity extends AppCompatActivity {
 
+    TextView createAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        initialization();
+
+
+        createAccount.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), SignUp_Activity.class);
+            startActivity(intent);
+        });
+
+    }
+
+    private void initialization() {
+        createAccount = findViewById(R.id.createNew);
     }
 }
--- app/src/main/java/com/geekym/knowdrowsy/authentication/SignUp_Activity.java
+++ app/src/main/java/com/geekym/knowdrowsy/authentication/SignUp_Activity.java
@@ -2,15 +2,37 @@ package com.geekym.knowdrowsy.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.geekym.knowdrowsy.R;
 
 public class SignUp_Activity extends AppCompatActivity {
 
+    Button createAccount;
+    TextView signIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        initialization();
+
+
+        signIn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), SignIn_Activity.class);
+            startActivity(intent);
+        });
+
+
+    }
+
+    private void initialization() {
+        createAccount = findViewById(R.id.createAccount);
+        signIn = findViewById(R.id.SignIn);
     }
 }
