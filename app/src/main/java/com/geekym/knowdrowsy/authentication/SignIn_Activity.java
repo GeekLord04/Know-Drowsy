6f281455eb00b4130060f71a8c346b54a3e65315
Binay Shaw <binayshaw7777@gmail.com>
2022-09-01 13:44:44 +0530

    (+) Splashscreen

---
+++ app/src/main/java/com/geekym/knowdrowsy/authentication/SignIn_Activity.java
@@ -0,0 +1,16 @@
+package com.geekym.knowdrowsy.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.geekym.knowdrowsy.R;
+
+public class SignIn_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
