957e28951c53d586e47df2aefb2866c35c86912c
Binay Shaw <binayshaw7777@gmail.com>
2022-09-04 11:16:43 +0530

    (+) Autologin Added

--- app/src/main/java/com/geekym/knowdrowsy/introduction/SplashScreen.java
+++ app/src/main/java/com/geekym/knowdrowsy/introduction/SplashScreen.java
@@ -9,51 +9,44 @@ import android.os.Handler;
 import com.geekym.knowdrowsy.MainActivity;
 import com.geekym.knowdrowsy.R;
 import com.geekym.knowdrowsy.authentication.SignIn_Activity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreen extends AppCompatActivity {
 
-    //FirebaseAuth mAuth;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-//        mAuth = FirebaseAuth.getInstance(); //initialize Firebase Auth
-//
-//        FirebaseUser currentUser = mAuth.getCurrentUser(); //Get the current user
-//
-//        if (currentUser == null)
-//
-//            SendUserToLoginActivity(); //If the user has not logged in, send them to On-Boarding Activity
-//
-//        else {
-//            //If user was logged in last time
+        mAuth = FirebaseAuth.getInstance(); //initialize Firebase Auth
 
-        new Handler().postDelayed(() -> {
+        FirebaseUser currentUser = mAuth.getCurrentUser(); //Get the current user
 
-            Intent loginIntent;
+        if (currentUser == null)
 
-//                if (currentUser.isEmailVerified())
-//                    loginIntent = new Intent(this, MainActivity.class); //If the user email is verified
-//                else
-            loginIntent = new Intent(this, SignIn_Activity.class); //If the user email is not verified
+            startActivity(new Intent(this, SignIn_Activity.class)); //If the user email is not verified
+            //If the user has not logged in, send them to On-Boarding Activity
 
-            loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(loginIntent);
-            finish();
-        }, 2000);
+        else {
+            //If user was logged in last time
 
+            new Handler().postDelayed(() -> {
+
+                Intent loginIntent;
+
+                if (currentUser.isEmailVerified())
+                    loginIntent = new Intent(this, MainActivity.class); //If the user email is verified
+                else
+                    loginIntent = new Intent(this, SignIn_Activity.class); //If the user email is not verified
+
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(loginIntent);
+                finish();
+            }, 2000);
+
+        }
     }
-
-    //If the user has not logged in, send them to On-Boarding Activity
-//    private void SendUserToLoginActivity() {
-//        new Handler().postDelayed(() -> {
-//            Intent loginIntent = new Intent(this, onboarding.class);
-//            Intent loginIntent = new Intent(this, SignIn_Activity.class);
-//            loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//            startActivity(loginIntent);
-//            finish();
-//        }, 2900);
-//    }
 }
